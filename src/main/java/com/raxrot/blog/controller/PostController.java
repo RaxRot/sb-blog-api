807dda67dac121780bd65c4e6ce8e9e12bdaa776
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-07 17:20:41 +0100

    add PostController

--- src/main/java/com/raxrot/blog/controller/PostController.java
+++ src/main/java/com/raxrot/blog/controller/PostController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+
     private final PostService postService;
     public PostController(PostService postService) {
         this.postService = postService;
