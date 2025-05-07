045de1e56c82e79b9b716e4f3c2b24558a45a6f3
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-07 16:00:23 +0100

    init

---
+++ src/test/java/com/raxrot/blog/SbBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
