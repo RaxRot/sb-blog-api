a78a80671d5fa86a50e8811b455d877533b55471
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-07 18:59:15 +0100

    add CommentDTO

---
+++ src/main/java/com/raxrot/blog/dto/CommentDTO.java
@@ -0,0 +1,27 @@
+package com.raxrot.blog.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Comment body is required")
+    private String body;
+}
