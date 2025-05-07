e79d84cb707b5da6a34924f7585adc0aeafd9f6b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-07 19:24:15 +0100

    add OpenAPIDefinition

--- src/main/java/com/raxrot/blog/SbBlogApiApplication.java
+++ src/main/java/com/raxrot/blog/SbBlogApiApplication.java
@@ -1,8 +1,22 @@
 package com.raxrot.blog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Blog REST API",
+                version = "1.0",
+                description = "REST API for managing blog posts and comments",
+                contact = @Contact(
+                        name = "RaxRot",
+                        email = "dasistperfektos@gmail.com",
+                        url = "https://github.com/RaxRot"
+                )
+        )
+)
 @SpringBootApplication
 public class SbBlogApiApplication {
 
