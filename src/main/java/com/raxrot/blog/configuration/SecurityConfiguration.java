bbc18cced112a4c48d208be7c258f104112a3341
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-09 18:07:08 +0100

    add CustomAccessDeniedHandler and CustomAuthenticationEntryPoint to configuration

--- src/main/java/com/raxrot/blog/configuration/SecurityConfiguration.java
+++ src/main/java/com/raxrot/blog/configuration/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.raxrot.blog.configuration;
 
+import com.raxrot.blog.exception.CustomAccessDeniedHandler;
+import com.raxrot.blog.exception.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,6 +20,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    public SecurityConfiguration(CustomAccessDeniedHandler accessDeniedHandler,CustomAuthenticationEntryPoint authenticationEntryPoint) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
@@ -27,6 +36,10 @@ public class SecurityConfiguration {
                 .requestMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
+        );
+        http.exceptionHandling(ex -> ex
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(authenticationEntryPoint)
         );
         http.httpBasic(Customizer.withDefaults());
         return http.build();
