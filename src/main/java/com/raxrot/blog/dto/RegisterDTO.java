85b6ed1b12a26450518f5cc51470558a0fcc841a
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 18:05:58 +0100

    add RegisterDTO

---
+++ src/main/java/com/raxrot/blog/dto/RegisterDTO.java
@@ -0,0 +1,30 @@
+package com.raxrot.blog.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDTO {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be at least 6 characters")
+    private String password;
+}
