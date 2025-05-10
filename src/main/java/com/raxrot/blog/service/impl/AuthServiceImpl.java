8b6f71c06c9ef255c08cc35552cb62af799bd3b5
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 18:02:43 +0100

    add logs

--- src/main/java/com/raxrot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/raxrot/blog/service/impl/AuthServiceImpl.java
@@ -2,12 +2,14 @@ package com.raxrot.blog.service.impl;
 
 import com.raxrot.blog.dto.LoginDTO;
 import com.raxrot.blog.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class AuthServiceImpl implements AuthService {
     private final AuthenticationManager authenticationManager;
@@ -17,6 +19,8 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String login(LoginDTO loginDTO) {
+        log.info("Login attempt for: {}", loginDTO.getUsernameOrEmail());
+
        Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
 
