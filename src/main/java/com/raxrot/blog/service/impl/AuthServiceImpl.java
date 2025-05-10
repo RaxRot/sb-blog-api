1c8112787f4d0498b85db9e2a0c7f9bda613538e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 17:41:19 +0100

    add AuthServiceImpl

---
+++ src/main/java/com/raxrot/blog/service/impl/AuthServiceImpl.java
@@ -0,0 +1,26 @@
+package com.raxrot.blog.service.impl;
+
+import com.raxrot.blog.dto.LoginDTO;
+import com.raxrot.blog.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private final AuthenticationManager authenticationManager;
+    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public String login(LoginDTO loginDTO) {
+       Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return  "User login successful";
+    }
+}
