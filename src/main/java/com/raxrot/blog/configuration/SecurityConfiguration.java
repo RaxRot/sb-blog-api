7ff5f6fd7ecbeefbc733dca10df8507e80b0a52a
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 16:11:19 +0100

    add methods to work with Db auth

--- src/main/java/com/raxrot/blog/configuration/SecurityConfiguration.java
+++ src/main/java/com/raxrot/blog/configuration/SecurityConfiguration.java
@@ -5,15 +5,14 @@ import com.raxrot.blog.exception.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,9 +21,16 @@ public class SecurityConfiguration {
 
     private final CustomAccessDeniedHandler accessDeniedHandler;
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
-    public SecurityConfiguration(CustomAccessDeniedHandler accessDeniedHandler,CustomAuthenticationEntryPoint authenticationEntryPoint) {
+    private final UserDetailsService userDetailsService;
+    public SecurityConfiguration(CustomAccessDeniedHandler accessDeniedHandler, CustomAuthenticationEntryPoint authenticationEntryPoint, UserDetailsService userDetailsService) {
         this.accessDeniedHandler = accessDeniedHandler;
         this.authenticationEntryPoint = authenticationEntryPoint;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+       return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -50,10 +56,12 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    /*
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails vlad= User.builder().username("vlad").password(passwordEncoder.encode("123")).roles("ADMIN").build();
         UserDetails daria= User.builder().username("daria").password(passwordEncoder.encode("123")).roles("USER").build();
         return new InMemoryUserDetailsManager(vlad,daria);
     }
+     */
 }
