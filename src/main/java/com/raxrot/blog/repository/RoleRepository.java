a7baffecc17a4a4fbab0df225baafbe7b5ffacc8
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 15:28:53 +0100

    add RoleRepository

---
+++ src/main/java/com/raxrot/blog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.raxrot.blog.repository;
+
+import com.raxrot.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
