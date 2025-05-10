1a83e4aa8ea0e096a41081895dc2d23490cc10ab
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 18:08:38 +0100

    add register method

--- src/main/java/com/raxrot/blog/service/AuthService.java
+++ src/main/java/com/raxrot/blog/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.raxrot.blog.service;
 
 import com.raxrot.blog.dto.LoginDTO;
+import com.raxrot.blog.dto.RegisterDTO;
 
 public interface AuthService {
     String login(LoginDTO loginDTO);
+    String register(RegisterDTO registerDTO);
 }
