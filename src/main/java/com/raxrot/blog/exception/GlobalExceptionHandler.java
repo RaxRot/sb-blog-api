a9dd88fb3e3e98cbbe197459554a3cfe671b1775
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-10 17:41:33 +0100

    add BadCredentialsException

--- src/main/java/com/raxrot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/raxrot/blog/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.raxrot.blog.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,5 +27,12 @@ public class GlobalExceptionHandler {
             errors.put(error.getField(), error.getDefaultMessage());
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentials(BadCredentialsException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Invalid username or password");
+        return new ResponseEntity<>(errors, HttpStatus.UNAUTHORIZED);
     }
 }
